7131ef9a49496bda6c225dfffbb72585bc87f467
Jessica Nugent <jessicanugent@Jessicas-MacBook-Air.local>
2020-04-05 14:16:39 -0700

    Started queries

---
+++ EmployeeSQL/queries.sql
@@ -0,0 +1,12 @@
+SELECT employees.emp_no,
+employees.last_name,
+employees.first_name,
+employees.gender,
+salaries.salary
+from employees
+INNER JOIN salaries ON
+employees.emp_no = salaries.emp_no;
+
+SELECT * 
+FROM employees
+WHERE hire_date = '1986-__-__';
